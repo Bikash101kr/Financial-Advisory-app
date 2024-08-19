7ed2a4ef2c536bc781943019427b4a902157710c
Bikash101 <57853841+Bikash101kr@users.noreply.github.com>
2024-08-19 20:39:29 +1000

    creating project

---
+++ js/gallery.js
@@ -0,0 +1,32 @@
+// Open the Modal
+function openModal() {
+    document.getElementById("galleryModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+    document.getElementById("galleryModal").style.display = "none";
+}
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
